4909b1b2a8bc8cc0cc392013f6d57c6c2c2e1438
Martin Burchell <martinb@aptivate.org>
2014-12-03 12:04:48 +0000

    Renamed 'thoth' with 'climate-tagger'

--- climate-tagger.php
+++ climate-tagger.php
@@ -1,19 +1,15 @@
 <?php
 /*
-Plugin Name: Thoth's Suggested Tags
-Plugin URI: http://wiki.github.com/edlab/Thoth-Suggested-Tags/
-Description: Recommends tags in a tag cloud based on post content as well as any existing tags in the database.
-Version: 1.3
-Author: Jimmy O'Higgins
+Plugin Name: Climate Tagger
+Description: Recommends tags in a tag cloud based on reegle tagging API.
+Version: 0.1
+Author: Aptivate, Jimmy O'Higgins
 */
 
-//TODO
-//Citations
-
-if(is_dir(WPMU_PLUGIN_DIR . '/thoth-suggested-tags'))
-	define('THOTH_INCLUDES', WPMU_PLUGIN_URL . '/thoth-suggested-tags');
+if(is_dir(WPMU_PLUGIN_DIR . '/climate-tagger'))
+	define('CLIMATE_TAGGER_INCLUDES', WPMU_PLUGIN_URL . '/climate-tagger');
 else
-	define('THOTH_INCLUDES', WP_PLUGIN_URL . '/thoth-suggested-tags');
+	define('CLIMATE_TAGGER_INCLUDES', WP_PLUGIN_URL . '/climate-tagger');
 
 //These words cannot be at the beginning or end of any tags
 $stop_words = str_replace(",", " ", " a,&amp,after,almost,also,am,among,an,and,any,are,aren't,as,at,be,because,between,been,began,both,but,by,can,cannot,could,dear,did,do,does,doesn't,don't,either,else,ever,every,for,from,gave,get,got,had,has,have,he,her,here,hers,him,his,how,however,i,if,in,into,instead,is,it,its,it's,least,let,like,likely,many,may,me,might,most,more,must,my,neither,nor,of,off,often,on,only,or,other,our,own,rather,really,said,say,says,shall,she,should,since,so,some,take,than,that,the,their,them,then,there,there's,these,they,this,tis,to,too,twas,us,wants,was,we,were,what,when,where,which,while,who,whom,why,will,with,would,yet,you,you'll,your,those ");
@@ -33,28 +29,28 @@ function add_box()
 
 function box_routine()
 {//Generates a tag cloud from tag list
-	
+
 	$limit = 12;
 	$tags_post = tag_list_generate_post();
 	$tags_db = tag_list_generate_db();
 	$tags_attach = tag_list_generate_attach();
-	
+
 	//No tags from post
 	if(empty($tags_post))
 	{
 		echo "Click 'Save Draft' to refresh tag suggestions.";
 		return;
 	}
-	
+
 	$tags_rec = $tags_post;
-	
+
 	//Merge with attachment tag recommendations
 	if(!empty($tags_attach))
 	{
 		$tags_rec = array_merge($tags_rec, $tags_attach);
 	}
-	
-	
+
+
 	foreach($tags_rec as $tag_name => &$tag_strength)
 	{
 		if(is_array($tags_db)
@@ -71,40 +67,40 @@ function box_routine()
 	}
 	arsort($tags_rec);
 	array_splice($tags_rec, $limit);
-	
+
 	//TAG CLOUD
 	//Init tag cloud variables
 	$min_size = 10;
 	$max_size = 24;
-	
+
 	$minimum_strength = min(array_values($tags_rec));
 	$maximum_strength = max(array_values($tags_rec));
-	
+
 	$spread = $maximum_strength - $minimum_strength;
 	if($spread == 0) $spread = 1;
-	
+
 	$step = ($max_size - $min_size)/($spread);
-	
+
 	//Print tag cloud
 	foreach($tags_rec as $tag_name => $tag_strength)
 	{
 		$size = $min_size + ($tag_strength - $minimum_strength) * $step;
-		
+
 		if(array_key_exists($tag_name, $tags_db))
 		{//Add asterisk to specify database suggestion
 			$new_name = $tag_name . '*';
 ?>
-		
+
 			<a href="#" style="font-size: <?php echo "$size"?>pt;" onClick="tag_add('<?php echo $tag_name; ?>');return false;"><?php echo "$new_name"?></a>
-			
+
 <?php
 		}
 		else
 		{//No match in database, render tag as is
 ?>
-		
+
 			<a href="#" style="font-size: <?php echo "$size"?>pt;" onClick="tag_add('<?php echo $tag_name; ?>');return false;"><?php echo "$tag_name"?></a>
-			
+
 <?php
 		}
 		//Space between tags
@@ -115,11 +111,11 @@ function box_routine()
 function tag_list_generate_db()
 {//Generates recommended tags from existing tags in the database
 	global $post, $stop_words;
-	
+
 	//Get tags from database
 	$tags = get_terms('post_tag', "get=all");
 	$tags_rec = array();
-	
+
 	//Convert $tags = array(tag structs) into $tags_rec = array($tag_name => $tag_strength)
 	foreach($tags as $tag_object)
 	{
@@ -139,7 +135,7 @@ function tag_list_generate_db()
 		$content = preg_replace('/[–-—]/', ' ', $content);
 		$content_exploded = explode(" ", $content);
 		$content_stemmed = array();
-		
+
 		//Explode db tags so that they match with partials in the post
 		foreach($tags_rec as $tag_name => $tag_strength)
 		{
@@ -157,7 +153,7 @@ function tag_list_generate_db()
 				}
 			}
 		}
-		
+
 		//Evaluate tags
 		foreach($tags_rec as $tag_name => $tag_strength)
 		{
@@ -179,25 +175,25 @@ The main loop in this function is based on a simple genetic sequencing algorithm
 function tag_list_generate_post()
 {
 	global $post, $stop_words, $single_words;
-	
+
 	$phrase_length_max = 4;
 	$phrases = array();
-	
+
 	//Initialize post content
 	$content = $post->post_title;
 	$content .=  " ".$post->post_content;
 	$content = strip_tags($content);
 	$content = preg_replace('/[\/"“”]/', '', $content);
 	$content = preg_replace('/[–-—]/', ' ', $content);
-	
+
 	//Split the content at these symbols, which delimit possible tags
 	$content_split = preg_split('/[–().,!?—;:…\n]/', $content);
-	
+
 	//Begin k-mer loop
 	foreach($content_split as $section)
 	{
 		$content_exploded = explode(" ", $section);
-		
+
 		for($phrase_length = 1; $phrase_length < $phrase_length_max; $phrase_length++)
 		{
 			for($phrase_start = 0; $phrase_start < count($content_exploded); $phrase_start++)
@@ -213,7 +209,7 @@ function tag_list_generate_post()
 				}
 				$phrase = trim($phrase);
 				//Phrase built
-				
+
 
 				if((str_word_count($phrase) == $phrase_length))
 				{//Evaluate phrase
@@ -221,10 +217,10 @@ function tag_list_generate_post()
 					$first_word = trim($phrase_exploded[0]);
 					$count = count($phrase_exploded);
 					$last_word = trim($phrase_exploded[$count-1]);
-					
+
 					$phrase = implode(" ", $phrase_exploded);
 					$phrase = trim($phrase);
-					
+
 					//Phrase cannot be empty
 					if(!empty($phrase_exploded))
 					{
@@ -254,18 +250,18 @@ function tag_list_generate_post()
 		}
 	}
 	//End k-mer loop
-	
+
 	//Begin postprocessing
 	$phrases = array_count_values($phrases);
 	arsort($phrases);
-	
+
 	//Discard weak tags
 	foreach($phrases as $phrase => $strength)
 	{
 		if($strength < 2)
 			unset($phrases[$phrase]);
 	}
-	
+
 	foreach($phrases as $phrase => &$strength)
 	{
 		//For single words
@@ -282,7 +278,7 @@ function tag_list_generate_post()
 					unset($phrases[$phrase]);
 				}
 			}
-			
+
 			//Check for plurals and match
 			$pluralized = $phrase.'s';
 			$pluralized = trim($pluralized);
@@ -291,7 +287,7 @@ function tag_list_generate_post()
 				$phrases[$pluralized] += $strength;
 				unset($phrases[$phrase]);
 			}
-			
+
 			//Check for duplicates
 			foreach($phrases as $phrase2 => $strength2)
 			{
@@ -315,7 +311,7 @@ function tag_list_generate_post()
 		$strength *= $multiplier;
 	}
 	arsort($phrases);
-	
+
 	return $phrases;
 }
 
@@ -329,26 +325,26 @@ function tag_list_generate_attach()
 	$audio_count = 0;
 	$video_strength = 4;
 	$audio_strength = 4;
-	
+
 	//Array of strings to associate with video
 	$video_strings = array('http://www.youtube.com/', 'http://vimeo.com/', 'http://www.dailymotion.com/', 'http://video.google.com/', '.avi', '.divx', '.flv', '.m4v', '.mov', '.mp4', '.mkv', '.mpg', '.ogm', '.swf', '.vob', '.wmv', '.xvid');
-	
+
 	//Array of strings to associate with audio
 	$audio_strings = array('.aac', '.aif', '.iff', '.m3u', '.mid', '.midi', '.mp3', '.mpa', '.wav', '.wma');
-	
+
 	//Search for video-associated strings in post
 	foreach($video_strings as $video_type)
 		$video_count += substr_count($content, $video_type);
-	
+
 	if($video_count)
 		$tags_rec['video'] = $video_count * $video_strength;
-	
+
 	foreach($audio_strings as $audio_type)
 		$audio_count += substr_count($content, $audio_type);
-	
+
 	if($audio_count)
 		$tags_rec['audio'] = $audio_count * $audio_strength;
-	
+
 	return $tags_rec;
 }
 
@@ -394,7 +390,7 @@ function is_proper_phrase($phrase)
 
 function admin_add_my_script()
 {
-	wp_enqueue_script('thoth-add-tag', THOTH_INCLUDES . '/thoth-add-tag.js', array('jquery'));
+	wp_enqueue_script('climate-tagger-add-tag', CLIMATE_TAGGER_INCLUDES . '/climate-tagger-add-tag.js', array('jquery'));
 }
 
 if(is_admin())
