a0339071f682b7a3888b0e08367986442b1c5398
Martin Burchell <martinb@aptivate.org>
2015-05-26 08:41:06 +0100

    Added test for content filter returning error

--- tests/test-plugin.php
+++ tests/test-plugin.php
@@ -9,4 +9,25 @@ class PluginTest extends WP_UnitTestCase {
 
 		$this->assertThat( $tagger, $this->isInstanceOf( 'ClimateTagger' ) );
 	}
+
+	public function test_returns_content_if_filter_error() {
+		$tagger = new ClimateTagger();
+
+		add_filter(
+			'climate_tagger_content',
+			array( $this, 'return_error' ), 1, 2 );
+
+		global $post;
+		$post = new StdClass();
+		$post->post_title = '';
+		$post->post_content = '';
+
+		$response = $tagger->get_reegle_tagger_response();
+
+		$this->assertThat( $response, $this->isInstanceOf( 'WP_Error' ) );
+	}
+
+	public function return_error() {
+		return new WP_Error();
+	}
 }
