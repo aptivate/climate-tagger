62018ba5ba5f3d2732fc8172ec162cee2df18b5d
Martin Burchell <martinb@aptivate.org>
2015-05-26 17:41:16 +0100

    Added test for post content sent to reegle

--- tests/test-plugin.php
+++ tests/test-plugin.php
@@ -32,10 +32,6 @@ class PluginTest extends WP_UnitTestCase {
 	public function test_text_includes_title() {
 		$tagger = new ClimateTagger();
 
-		add_filter(
-			'climate_tagger_content',
-			array( $this, 'return_error' ), 1, 2 );
-
 		$post = $this->get_new_post();
 		$post->post_title = 'FEATURE: Three Steps to Decarbonising Development for a Zero-Carbon Future';
 
@@ -50,6 +46,28 @@ class PluginTest extends WP_UnitTestCase {
 			$this->stringContains(
 				'FEATURE: Three Steps to Decarbonising Development for a Zero-Carbon Future'
 			)
+		);
+	}
+
+	public function test_text_includes_post_content() {
+		$tagger = new ClimateTagger();
+
+		$post = $this->get_new_post();
+
+		$content = <<<EOT
+A new World Bank report lays out three steps for a smooth transition to a zero-carbon future. Through data, examples and policy advice, it aims to help countries makes the shift. It tells us that to prevent temperatures from rising more than 2 degrees Celsius, the world will need to transform its energy uses and electricity from clean energy sources will play an important role.
+EOT;
+		$post->post_content = $content;
+
+		$tagger->get_reegle_tagger_response();
+
+		global $_CLIMATE_TAGGER_MOCK_POST;
+
+		$text = $_CLIMATE_TAGGER_MOCK_POST['text'];
+
+		$this->assertThat(
+			$text,
+			$this->stringContains( $content	)
 		);
 	}
 
